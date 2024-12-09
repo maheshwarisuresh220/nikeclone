6e3c0bce4208dff2a2eb0ca35a349f9b61a5d143
Suresh <maheshwarisuresh220@gmail.com>
2024-12-09 11:21:23 +0500

    navbarcorrection

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -2,59 +2,86 @@ import Image from "next/image";
 import { FaSearch, FaHeart, FaShoppingBag } from "react-icons/fa";
 import Link from "next/link";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Navbar = () => {
   return (
-    <div className="bg-white">
-      {/* Main container with flex to align the logo and navbar items */}
-      <div className="flex items-center justify-between py-6 px-8 shadow-md">
-        {/* Nike logo */}
-        <Image
-          src="/nike.png"
-          alt="Nike logo"
-          width={70}
-          height={70}
-          className="mt-4"
-        />
+    <div className="bg-white w-full py-[10px]">
+      {/* Main container */}
+      <div className="flex items-center justify-between py-6 px-8 shadow-md w-full">
+        {/* Nike logo with Sheet Trigger */}
+        <Sheet>
+          <SheetTrigger>
+            <Image
+              src="/nike.png"
+              alt="Nike logo"
+              width={70}
+              height={70}
+              className="cursor-pointer"
+            />
+          </SheetTrigger>
+          <SheetContent className="w-full max-w-none">
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              <SheetDescription>
+                Navigate Through Website
+              </SheetDescription>
+            </SheetHeader>
+            <ul className="flex flex-col space-y-4 mt-4">
+  <li>
+    <Link
+      href="/"
+      className="hover:text-blue-800 hover:underline transition-colors duration-200 inline-block"
+    >
+      Home
+    </Link>
+  </li>
+  <li>
+    <Link
+      href="/allproducts"
+      className="hover:text-blue-800 hover:underline transition-colors duration-200 inline-block"
+    >
+      All Products
+    </Link>
+  </li>
+  <li className="hover:text-blue-800 hover:underline transition-colors duration-200 inline-block">
+    Men
+  </li>
+  <li className="hover:text-blue-800 hover:underline transition-colors duration-200 inline-block">
+    Women
+  </li>
+  <li className="hover:text-blue-800 hover:underline transition-colors duration-200 inline-block">
+    Kids
+  </li>
+  <li className="hover:text-blue-800 hover:underline transition-colors duration-200 inline-block">
+    Sale
+  </li>
+</ul>
 
-        {/* Navigation menu */}
+          </SheetContent>
+        </Sheet>
+
+        {/* Navigation menu for larger screens */}
         <div className="hidden md:flex font-semibold mt-4 ml-[150px]">
           <ul className="flex space-x-8 items-center justify-center text-lg">
-            {/* Dropdown for Men */}
-            <li className="relative cursor-pointer hover:underline">
-            <DropdownMenu>
-  <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-  <DropdownMenuContent>
-    <DropdownMenuLabel>My Account</DropdownMenuLabel>
-    <DropdownMenuSeparator />
-    <DropdownMenuItem>Profile</DropdownMenuItem>
-    <DropdownMenuItem>Billing</DropdownMenuItem>
-    <DropdownMenuItem>Team</DropdownMenuItem>
-    <DropdownMenuItem>Subscription</DropdownMenuItem>
-  </DropdownMenuContent>
-</DropdownMenu>
-
+            <li className="cursor-pointer hover:underline">
+              <Link href="/allproducts">All Products</Link>
             </li>
-
-            {/* Other menu items */}
-            <li className="cursor-pointer hover:underline"><Link href="/allproducts">All Products</Link></li>
+            <li className="cursor-pointer hover:underline">Men</li>
             <li className="cursor-pointer hover:underline">Women</li>
             <li className="cursor-pointer hover:underline">Kids</li>
             <li className="cursor-pointer hover:underline">Sale</li>
           </ul>
         </div>
 
-        {/* Container for search bar and icons */}
+        {/* Search and icon buttons */}
         <div className="flex items-center space-x-5">
-          {/* Search Bar */}
           <div className="relative w-[180px] sm:w-[220px] md:w-[250px]">
             <input
               type="text"
@@ -63,39 +90,14 @@ const Navbar = () => {
             />
             <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
           </div>
-
-          {/* Add to Cart Button */}
           <Link href="/cartpage">
             <button className="p-3 bg-blue-600 rounded-full hover:bg-blue-700 focus:outline-none">
               <FaShoppingBag className="text-white hover:text-gray-200" />
             </button>
           </Link>
-
-          {/* Heart-Shaped Button */}
           <button className="p-3 bg-gray-200 rounded-full hover:bg-gray-300 focus:outline-none">
             <FaHeart className="text-red-500" />
           </button>
-        </div>
-      </div>
-
-      {/* Mobile Menu Toggle for Smaller Screens */}
-      <div className="md:hidden flex justify-between items-center px-6 py-4">
-        {/* Hamburger Icon for Mobile Menu */}
-        <div className="space-y-2 cursor-pointer">
-          <div className="w-6 h-1 bg-gray-600"></div>
-          <div className="w-6 h-1 bg-gray-600"></div>
-          <div className="w-6 h-1 bg-gray-600"></div>
-        </div>
-
-        {/* Mobile Menu Items */}
-        <div className="hidden mobile-menu md:hidden absolute bg-white w-full h-screen top-0 left-0">
-          <ul className="flex flex-col items-center justify-center text-lg space-y-4">
-            <li><Link href="/allproducts">All Products</Link></li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
-            <li>Sale</li>
-          </ul>
         </div>
       </div>
     </div>
